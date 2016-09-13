d712c1667a183e99dc360aa59d0af85bfcc999ac
Galaxy9572 <ljy565880512@163.com>
2016-09-13 13:47:03 +0800

    2016/09/13：第二次提交，添加二维码组件，廖俊瑶

--- app/src/main/java/com/orange/ifitdiet/activity/MainActivity.java
+++ app/src/main/java/com/orange/ifitdiet/activity/MainActivity.java
@@ -31,6 +31,7 @@ import com.orange.ifitdiet.fragment.HealthFragment;
 import com.orange.ifitdiet.fragment.LocateFragment;
 import com.orange.ifitdiet.fragment.RecommendFragment;
 import com.orange.ifitdiet.util.LocateUtil;
+import com.zf.myzxing.CaptureActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -223,7 +224,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_mymsg) {
-            startActivity(new Intent().setClass(MainActivity.this, MyMsgActivity.class));
+            startActivity(new Intent().setClass(MainActivity.this, CaptureActivity.class));
         } else if (id == R.id.nav_personal_info) {
             startActivity(new Intent().setClass(MainActivity.this, PersonalInfoActivity.class));
         } else if (id == R.id.nav_share) {
